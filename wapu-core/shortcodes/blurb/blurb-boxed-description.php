6cf868452290558f96cd26bce56f45ebdecd7843
mjhead <andremjhead@gmail.com>
2017-06-06 17:05:17 +0300

    ADD: target attributes for links in blurb

--- wapu-core/shortcodes/blurb/blurb-boxed-description.php
+++ wapu-core/shortcodes/blurb/blurb-boxed-description.php
@@ -4,14 +4,14 @@
  */
 ?>
 <div class="blurb <?php echo $class; ?>" <?php echo $id; ?>>
-	<?php $this->html( $link, '<a href="%1$s" class="blurb__link">' ) ?> 
+	<?php $this->html( $link, '<a href="%1$s" class="blurb__link"%2$s>', array( $target ) ); ?>
 		<?php echo $img_tag; ?>
 		<?php $this->html( $font_icon, '<i class="nc-icon-outline %s"></i>' ); ?>
-	</a>
+	<?php $this->html( $link, '</a>' ); ?>
 	<div class="blurb_description">
-		<?php 
-			$this->html( $title, '<h6 class="blurb__title">%s</h6>' ); 
-			$this->html( $text, '<div class="blurb__text">%s</div>' ); 
+		<?php
+			$this->html( $title, '<h6 class="blurb__title">%s</h6>' );
+			$this->html( $text, '<div class="blurb__text">%s</div>' );
 		?>
 	</div>
 </div>
