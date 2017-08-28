22752fcec196c7f59cf358849da3b9cbf609456f
mjhead <andremjhead@gmail.com>
2017-08-28 17:40:20 +0300

    FIX: blog template

--- base-pages.php
+++ base-pages.php
@@ -1,4 +1,9 @@
 <?php get_header('pages'); ?>
+	<?php
+		if ( wapu_need_rewrite() ) {
+			wapu_breadcrumbs();
+		}
+	?>
 	<div class="site-content_wrap container">
 		<div class="row">
 			<div id="primary" class="<?php echo wapu_layout_class( 'content' ); ?>">
@@ -8,6 +13,11 @@
 					</div>
 				</main><!-- #main -->
 			</div><!-- #primary -->
+			<?php
+				if ( wapu_need_rewrite() ) {
+					get_sidebar();
+				}
+			?>
 		</div><!-- .row -->
 	</div><!-- .container -->
 <?php get_footer(); ?>
