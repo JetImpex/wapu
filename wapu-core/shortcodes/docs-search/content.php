6a3fc8bb5cc80abcfacae5e853f2783909eef9b1
mjhead <andremjhead@gmail.com>
2017-09-08 12:32:26 +0300

    UPD: docs search placeholder

--- wapu-core/shortcodes/docs-search/content.php
+++ wapu-core/shortcodes/docs-search/content.php
@@ -8,7 +8,7 @@
 <div class="docs-search <?php echo $class; ?>" <?php echo $id; ?>>
 	<h5><?php esc_html_e( 'Search for documentation:', 'wapu' ); ?></h5>
 	<div class="docs-search__form">
-		<input type="text" name="_docs_query" class="docs-search__input" placeholder="<?php esc_html_e( 'Enter theme name or template ID', 'wapu' ); ?>">
+		<input type="text" name="_docs_query" class="docs-search__input" placeholder="<?php esc_html_e( 'Enter theme name', 'wapu' ); ?>">
 		<button class="docs-search__submit" type="submit" data-init="docs"><?php
 			esc_html_e( 'Find', 'wapu' );
 		?></button>
