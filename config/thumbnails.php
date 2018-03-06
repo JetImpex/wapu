86e2e0620802fd751064675255595cdafecfceaf
mjhead <andremjhead@gmail.com>
2018-03-06 18:33:36 +0200

    UPD: header cart and account

--- config/thumbnails.php
+++ config/thumbnails.php
@@ -4,10 +4,10 @@
  */
 
 return array(
-	'search-thumbnail' => array( 'width' => 86, 'height' => 66, 'crop' => true ),
+	'search-thumbnail'  => array( 'width' => 86, 'height' => 66, 'crop' => true ),
 	'smaller-thumbnail' => array( 'width' => 418, 'height' => 237, 'crop' => true ),
-	'small-thumbnail' => array( 'width' => 418, 'height' => 323, 'crop' => true ),
-	'middle-thumbnail' => array( 'width' => 652, 'height' => 384, 'crop' => true ),
-	'post-thumbnail' => array( 'width' => 886, 'height' => 552, 'crop' => false ),
-	'large-thumbnail' => array( 'width' => 1354, 'height' => 798, 'crop' => true ),
+	'small-thumbnail'   => array( 'width' => 418, 'height' => 323, 'crop' => true ),
+	'middle-thumbnail'  => array( 'width' => 652, 'height' => 384, 'crop' => true ),
+	'post-thumbnail'    => array( 'width' => 886, 'height' => 552, 'crop' => false ),
+	'large-thumbnail'   => array( 'width' => 1354, 'height' => 798, 'crop' => true ),
 );
