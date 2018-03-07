79e6f51607022d77b39ed4f743861864c32ccabc
mjhead <andremjhead@gmail.com>
2018-03-07 18:05:58 +0200

    UPD: theme listing styles

--- config/thumbnails.php
+++ config/thumbnails.php
@@ -10,4 +10,5 @@ return array(
 	'middle-thumbnail'  => array( 'width' => 652, 'height' => 384, 'crop' => true ),
 	'post-thumbnail'    => array( 'width' => 886, 'height' => 552, 'crop' => false ),
 	'large-thumbnail'   => array( 'width' => 1354, 'height' => 798, 'crop' => true ),
+	'theme-thumbnail'   => array( 'width' => 350, 'height' => 250, 'crop' => true ),
 );
