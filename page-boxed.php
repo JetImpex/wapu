94752a028d3e792ed7a08f0510c52680f9ca0aad
mjhead <andremjhead@gmail.com>
2018-03-20 11:00:59 +0200

    ADD: Boxed page template

---
+++ page-boxed.php
@@ -0,0 +1,13 @@
+<?php
+/* Template Name: Page Boxed */
+
+while ( have_posts() ) : the_post();
+
+	get_template_part( 'template-parts/content', 'page' );
+
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+		comments_template();
+	endif;
+
+endwhile; // End of the loop.
